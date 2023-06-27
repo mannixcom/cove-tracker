3c3243f4ab131a9df8f19f1ea6d96e8c35eb115f
mannixcom <mannixcom@gmail.com>
2023-06-27 21:53:08 +0100

    update spelling

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>Mannix cove test newß</div>
+     <div>Mannix cove test news</div>
     </main>
   )
 }
