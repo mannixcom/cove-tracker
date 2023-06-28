9231ee337c44e3e119af0ae04623de1cf9ebe2e4
mannixcom <mannixcom@gmail.com>
2023-06-28 12:52:31 +0100

    update the home page git gmap embed

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,35 @@
+import Link from 'next/link';
 import React from 'react'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Mannix cove test news</div>
+export default function Home() { 
+const latitude = 52.13909351325254;
+const longitude = -7.015760733094569;
+const apiKey = process.env.GMAPAPI;
+const googleMapsUrl = `https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${latitude},${longitude}&zoom=16`;
 
-    </main>
-  )
+return (
+  <main className="flex min-h-screen flex-col items-center p-24">
+    <div>
+      <h2>Portally Cove Report</h2>
+      <Link 
+      href='tide'
+      style={{
+        textDecoration: 'underline'
+      }}
+      >
+        Weather and Tide Data</Link>
+      </div>
+    <div className="mt-10">
+      <iframe 
+        width="600" 
+        height="450" 
+        frameborder="0" 
+        style={{border: 0}} 
+        src={googleMapsUrl} 
+        allowfullscreen=""
+        loading="lazy"
+      />
+    </div>
+  </main>
+);
 }
