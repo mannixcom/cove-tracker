f1e0eb083c17aebe3601deed5ac295c18a4c6462
mannixcom <mannixcom@gmail.com>
2023-07-04 21:59:39 +0100

    fix build errors

--- src/pages/tide.js
+++ src/pages/tide.js
@@ -2,7 +2,7 @@ import React from "react";
 import { format } from "date-fns";
 import { fetchTides } from "@/api/api-utils";
 
-export default function Home({ todaysTides, currentTide }) {
+export default function Home({ todaysTides }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
@@ -13,16 +13,6 @@ export default function Home({ todaysTides, currentTide }) {
             {format(new Date(tide.time), "h:mm:ss a")}, Type: {tide.type}
           </p>
         ))}
-        {currentTide && (
-          <div>
-            <h3>Current Tide</h3>
-            <p>
-              Height: {currentTide.height}, Time:{" "}
-              {format(new Date(currentTide.time), "h:mm:ss a")}, Type:{" "}
-              {currentTide.type}
-            </p>
-          </div>
-        )}
       </div>
     </main>
   );
@@ -43,16 +33,12 @@ export async function getStaticProps() {
     const today = new Date().toISOString().split("T")[0];
     return tide.time.startsWith(today);
   });
-  const currentTide = todaysTides.find((tide) => {
-    const tideTime = new Date(tide.time).getTime();
-    return tideTime > Date.now();
-  });
+
 
 
   return {
     props: {
       todaysTides,
-      currentTide,
     },
     revalidate: 4 * 60 * 60, // Revalidate every 4 hours
   };
