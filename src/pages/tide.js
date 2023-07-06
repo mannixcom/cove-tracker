54d96ffce295e8cca940d665cbd5731f02ebe4f2
mannixcom <mannixcom@gmail.com>
2023-07-06 16:05:06 +0100

    add new idea for cove visualizations

--- src/pages/tide.js
+++ src/pages/tide.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { fetchTides } from "@/api/api-utils"
 import TideChart from "@/components/TideChart";
+import TidePlot from "@/components/TidePlot";
 
-export default function Home({ todaysTides }) {
+export default function Home({ todaysTides, allTides }) {
  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h2>Todays Tide Information</h2>
+        <>
         <TideChart todaysTides={todaysTides}/>
+        </>
+        <h2>The Tide Activity for the Coming Week</h2>
+        <>
+        <TidePlot allTides={allTides}/>
+        </>
       </div>
     </main>
   );
@@ -21,14 +28,18 @@ export async function getStaticProps() {
   const response = await fetchTides(lat, lng)
 
   const todaysTides = response.heights.filter((tide) => {
-    // Modify this to match your timezone and formatting requirementsx
+
     const today = new Date().toISOString().split("T")[0];
     return tide.date.startsWith(today);
   });
 
+  const allTides = response.heights
+  // console.log(allTides)
+
   return {
     props: {
       todaysTides,
+      allTides,
     },
     revalidate: 12 * 60 * 60, // Revalidate every 12 hours
   };
