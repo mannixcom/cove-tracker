f71eda4a382d09b2d32196fd4f56fa5f585db21d
mannixcom <mannixcom@gmail.com>
2023-07-17 12:00:22 +0100

    update the activity matrix

--- src/api/cove-rating.js
+++ src/api/cove-rating.js
@@ -2,11 +2,15 @@ export function generateActivityRatings(data) {
   let seriesData = {
     'Swimming': [],
     'Caving': [],
+    'Fishing': [],
+    'Seaweed Picking': [],
+    // 'Rockpools': [],
   };
 
   data.forEach(({ tide, date }) => {
-    let swimmingRating, cavingExploringRating;
-
+    let swimmingRating, cavingExploringRating, fishingRating, seaweedRating;
+    let formatDate = new Date(date);
+    let hour = formatDate.getUTCHours();
     // Logic for rating swimming
     if ((tide >= 0.3)) {
         swimmingRating = 3;
@@ -25,15 +29,39 @@ export function generateActivityRatings(data) {
         cavingExploringRating = 2;
     }
 
-    seriesData['Swimming'].push({
-      x: date,
-      y: swimmingRating,
-    });
+    //Logic for rating fishing
+    if((tide > 0.9) && (hour >= 17 && hour <22)){
+      fishingRating = 3;
+    } else {
+      fishingRating = 1;
+    }
+
+    //Logic for rating Seaweed Picking
+      if((tide < -1.2)){
+        seaweedRating = 3;
+      } else {
+        seaweedRating = 1;
+      }
 
     seriesData['Caving'].push({
       x: date,
       y: cavingExploringRating,
     });
+
+    seriesData['Fishing'].push({
+      x: date,
+      y: fishingRating,
+    });
+
+    seriesData['Swimming'].push({
+      x: date,
+      y: swimmingRating,
+    });
+
+    seriesData['Seaweed Picking'].push({
+      x: date,
+      y: seaweedRating,
+    })
 
   });
 
