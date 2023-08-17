f178dd64b1cc79a625dbd6f7b15d0a06f1091c95
Mannix Carney <mannixcom@gmail.com>
2023-08-17 13:02:11 +0100

    test setup

--- src/api/api-utils.js
+++ src/api/api-utils.js
@@ -5,6 +5,7 @@ export async function fetchTides(lat, lng) {
   const response = await fetch(
     `https://www.worldtides.info/api/v3?heights&extremes&date=${start}&lat=${lat}&lon=${lng}&days=7&key=${process.env.WORLDTIDEAPI}`
   );
+
   if (!response.ok) {
     const message = `An error has occured: ${response.status}`;
     throw new Error(message);
