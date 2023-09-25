a29bc1870b21a6135b2779cc4b0eb1b6e91cce2a
rudyruby <44081612+rudyruby@users.noreply.github.com>
2023-09-25 10:17:47 +0100

    feat(topnav): slogan, sticky, border (#40)
    Signed-off-by: Rudolf Batke <rudolf@batke.eu>

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,28 +2,27 @@ import React from 'react';
 import '../styles/global.css';
 import { Inter } from 'next/font/google';
 import TitleBox from '../components/TitleBox';
-import icon from './icon.png';
-import Image from 'next/image';
+import TopBar from '../components/TopBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-	title: 'Cove Tracker',
-	description: 'Plan your cove visits',
+  title: 'Cove Tracker',
+  description: 'Plan your cove visits',
 };
 
 export default function Layout({ children }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<div style={{ backgroundColor: '#F7F9F9' }}>
-					<div style={{ margin: '0 auto', maxWidth: '90vw' }}>
-						<Image src={icon} alt="icon" width={35} height={35} style={{ marginTop: '8px' }} />
-						<TitleBox />
-						{children}
-					</div>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={inter.className} style={{ margin: 'unset' }}>
+        <div style={{ backgroundColor: '#F7F9F9' }}>
+          <div style={{ margin: '0 auto', maxWidth: '90vw' }}>
+            <TopBar />
+            <TitleBox />
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
